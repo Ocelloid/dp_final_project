97ae4fecd8866458dfd1e35a81c1df9ed70ceb08
Nikita Grebnev <n.a.grebnev@gmail.com>
2021-04-11 00:47:34 -0400

    About placeholder

--- front/src/components/About/index.js
+++ front/src/components/About/index.js
@@ -1,10 +1,61 @@
 import React from 'react';
+import nikita from "../../assets/nikita.png";
+import default_picture from "../../assets/default.png";
 import "./style.scss";
 
 export default class About extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            people: [
+                {
+                    name: "Nikita Grebnev",
+                    picture_link: nikita,
+                    description: "Front-end developer specialising on ReactJS, BDAT student at Georgian."
+                },
+                {
+                    name: "Shane Abeysekera",
+                    picture_link: default_picture,
+                    description: ""
+                },
+                {
+                    name: "Clancy Francisbhai Macwanv",
+                    picture_link: default_picture,
+                    description: ""
+                },
+            ]
+        }
+    }
+
     render() {
-        return <div>
+        let {people} = this.state;
+        return <div className={"about-wrapper"}>
             <h1>About us</h1>
+            {!!people.length && people.map(person => <AboutUnit person={person}/>)}
         </div>;
+    }
+}
+
+class AboutUnit extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            person: props.person
+        }
+    }
+
+    render() {
+        let {person} = this.state;
+        return <div className={"about-unit"}>
+            <div className={"about-picture-wrapper"}>
+                <img src={person.picture_link} alt={"profile picture"}/>
+            </div>
+            <div className={"about-profile"}>
+                <h2>{person.name}</h2>
+                <span className={"about-description"}>
+                    {person.description}
+                </span>
+            </div>
+        </div>
     }
 }
